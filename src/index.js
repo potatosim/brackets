a2a453f2522cdf42ebf1ff29f21be80371d57ec6
potatosim <devildriver2310@gmail.com>
2022-07-14 17:54:56 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let arr = Array.from(str);
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if (
+        arr[i] === bracketsConfig[j][0] &&
+        arr[i + 1] === bracketsConfig[j][1]
+      ) {
+        arr.splice(i, 2);
+        i -= 2;
+      }
+    }
+  }
+  return !arr.length;
+};
